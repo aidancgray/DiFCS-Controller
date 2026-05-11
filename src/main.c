b423f14c866d1fb74f0061cd677b613316c2a520
Aidan Gray <aidancgray@gmail.com>
2026-05-11 16:47:20 -0400

    clean up

--- src/main.c
+++ src/main.c
@@ -13,7 +13,6 @@
 void main()
 {
     int16 loopDelay = 100;
-    int16 controlCounter = 0;
     int16 debugCounter = 0;
    
     char pBuff[100];
@@ -34,30 +33,17 @@ void main()
         if (adcVals[chX].homeFlag) home_axis(chX);
         if (adcVals[chY].homeFlag) home_axis(chY);
                 
-        internal_monitor_task();   //Update monitored voltage and currents etc
-        sensor_monitor_task();     //get magnetoresistive sensor data
-      
-//!        if (controlCounter >= 2){
-//!            control_task();
-//!            controlCounter = 0;
-//!        }else controlCounter++;
+        internal_monitor_task();
+        sensor_monitor_task();
         control_task();
-        
-        serial_task();             
-        command_handler_task();    //execute commands
+        serial_task();     
+        command_handler_task();
         
         #ifdef DEBUG_1
         pBuff[0] = '\0';
-        if (debugCounter >= 0){
-            static int8 ch = 0;
-            sprintf(pBuff+strlen(pBuff), "$,%u,%.0f,%.0f;", ch+1, adcVals[ch].sinCounts, adcVals[ch].cosCounts);
-//!            sprintf(pBuff+strlen(pBuff), "CNT,%u,%.0f,%.0f;", ch+1, adcVals[ch].sinCounts, adcVals[ch].cosCounts);
-//!            sprintf(pBuff+strlen(pBuff), "POS,%u,%3.3f;", ch+1, adcVals[ch].pReal);
-//!            if ( dacVals[ch].invV ) sprintf(pBuff+strlen(pBuff), "OUT,%u,-,%Lu;", ch+1, dacVals[ch].ipVal);
-//!            else                    sprintf(pBuff+strlen(pBuff), "OUT,%u,+,%Lu;", ch+1, dacVals[ch].ipVal);
-            ch = !ch;
-            debugCounter = 0;
-        }else debugCounter++;
+        static int8 ch = 0;
+        sprintf(pBuff+strlen(pBuff), "$,%u,%.0f,%.0f;", ch+1, adcVals[ch].sinCounts, adcVals[ch].cosCounts);
+        ch = !ch;
         serial_out(pBuff);
         #endif
       
